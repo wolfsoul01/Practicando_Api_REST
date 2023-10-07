70c0419a5221b33053b5bf540e7c79752c01b1bb
wolfsoul <alejandrof011021@gmail.com>
2023-10-07 12:32:37 -0400

    middelware de prueba y page 404

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
-import express from 'express'
+import express from "express";
 import { readJSON } from "./utils/readJSON.js";
 const PORT = 3000;
 
+const app = express();
 
-const app = express()
+function middelware(req, res, next) {
+  console.log("Prueba 1");
+  next();
+}
 
-app.get('/',(req,res)=>{
 
-    const data = readJSON('../db/local/db.json')
+app.get("/", [middelware], (req, res) => {
 
-    res.json(data)
+    res.send('<h1>Hola mundo </h1>')
+});
+
+app.get("/user", (req, res) => {
+  const data = readJSON("../db/local/db.json");
+
+  res.json(data);
+});
+
+
+app.use((req,res)=>{
+
+    res.send('<h1>404 Page not Found</h1>')
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server rining on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server rining on http://localhost:${PORT}`);
+});
