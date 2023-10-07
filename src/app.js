049560279db43ac7453a893ceda4593f25499b79
wolfsoul <alejandrof011021@gmail.com>
2023-10-07 12:20:05 -0400

    MIgrando a express

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-import http from "node:http";
+import express from 'express'
 import { readJSON } from "./utils/readJSON.js";
 const PORT = 3000;
 
-const app = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
 
-  const data = readJSON("../db/local/db.json");
+const app = express()
 
-  res.end(JSON.stringify(data))
-});
+app.get('/',(req,res)=>{
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    const data = readJSON('../db/local/db.json')
+
+    res.json(data)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server rining on http://localhost:${PORT}`);
+})
