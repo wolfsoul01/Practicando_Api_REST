722fee2e1538b97d7af524a8b517a58402b41ecf
wolfsoul <alejandrof011021@gmail.com>
2023-10-07 22:32:00 -0400

    creando el modelo local para los usuarios

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,16 @@
-import { readJSON } from "../utils/readJSON.js";
-import { validarUser ,partialValidUser} from "../schema/userZod.js";
-import { randomUUID } from "crypto";
-
-const users = readJSON("../db/local/db.json") || [];
+import { validarUser, partialValidUser } from "../schema/userZod.js";
+import { UserModel } from "../model/user.model.js";
 
 export class UserController {
   static async getAll(req, res) {
+    const users = await UserModel.getAllUser();
     res.json(users);
   }
 
   static async getById(req, res) {
     const { id } = req.params;
 
-    const userId = users.find((item) => item.Id_user === id);
+    const userId = await UserModel.getById(id);
     res.json(userId);
   }
 
@@ -23,28 +21,20 @@ export class UserController {
 
     if (!data.success) res.json({ message: "Error ", err: data.error });
 
-    const newUser = {
-      id_user: randomUUID(),
-      create: Date.now(),
-      ...data.data,
-    };
-
-    res.json(newUser);
+    const { id_user, ...restUser } = await UserModel.createUser(data.data);
+    res.json(restUser);
   }
 
   static async updateUser(req, res) {
     const { id } = req.params;
-    const result  = partialValidUser(req.body)
+    const result = partialValidUser(req.body);
 
-   if(!result.success) res.json({message:'Error',err:result.error})
+    if (!result.success) res.json({ message: "Error", err: result.error });
 
-
-    const userIndex = users.findIndex((user) => user.Id_user === id);
-
-    const newUser= {
-        ...users[userIndex],
-        ...result
-    }
-    res.json(newUser)
+    const { id_user, ...restUser } = await UserModel.udateUser({
+      userData: result.data,
+      id,
+    });
+    res.json(restUser);
   }
 }
---
+++ src/model/user.model.js
@@ -0,0 +1,39 @@
+import { readJSON } from "../utils/readJSON.js";
+import { randomUUID } from "crypto";
+
+const users = readJSON("../db/local/db.json") || [];
+
+export class UserModel {
+  static async getAllUser() {
+    return users;
+  }
+
+  static async getById(id) {
+    return users.find((item) => item.Id_user === id);
+  }
+
+  static async createUser(userData) {
+    const newUser = {
+      id_user: randomUUID(),
+      create: Date.now(),
+      ...userData,
+    };
+    users.push(newUser);
+    return newUser;
+  }
+
+  static async getIndexById(id) {
+    return users.findIndex((user) => user.Id_user === id);
+  }
+
+  static async udateUser({ userData, id }) {
+    const userIndex = await this.getIndexById(id);
+
+    const newUser = {
+      ...users[userIndex],
+      ...userData,
+    };
+    users[userIndex] = newUser;
+    return newUser;
+  }
+}
